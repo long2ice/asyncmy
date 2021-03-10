4363f3a50bfb9ca7bb1d4a6a0c690df2b0d7a86f
long2ice <long2ice@gmail.com>
2021-03-10 20:14:38 +0800

    Fix build error

--- build.py
+++ build.py
@@ -1,37 +1,9 @@
-import os
-
-# See if Cython is installed
-try:
-    from Cython.Build import cythonize
-# Do nothing if Cython is not available
-except ImportError:
-    # Got to provide this function. Otherwise, poetry will fail
-    def build(setup_kwargs):
-        pass
-# Cython is installed. Compile
-else:
-    from setuptools import Extension
-    from setuptools.dist import Distribution
-    from distutils.command.build_ext import build_ext
+from Cython.Build import cythonize
+from distutils.command.build_ext import build_ext
 
 
-    # This function will be executed in setup.py:
-    def build(setup_kwargs):
-        # The file you want to compile
-        extensions = [
-            "asyncmy/*.pyx",
-        ]
-
-        # gcc arguments hack: enable optimizations
-        os.environ['CFLAGS'] = '-O3'
-
-        # Build
-        setup_kwargs.update({
-            'ext_modules': cythonize(
-                extensions,
-                language_level=3,
-                gdb_debug=True,
-                compiler_directives={'linetrace': True},
-            ),
-            'cmdclass': {'build_ext': build_ext}
-        })
+def build(setup_kwargs):
+    setup_kwargs.update({
+        'ext_modules': cythonize("asyncmy/*.pyx"),
+        'cmdclass': {'build_ext': build_ext}
+    })
