9092ff05860e1bd211b2f97b5d443e1d6bb3c6f5
long2ice <long2ice@gmail.com>
2021-03-11 20:25:05 +0800

    remove struct

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ from distutils.command.build_ext import build_ext
 
 def build(setup_kwargs):
     setup_kwargs.update({
-        'ext_modules': cythonize(["asyncmy/*.pyx", "asyncmy/struct/*.pyx"]),
+        'ext_modules': cythonize(["asyncmy/*.pyx"]),
         'cmdclass': {'build_ext': build_ext}
     })
