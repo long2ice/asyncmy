be3efbd431fdc7493c453d9acff78a86f542cc41
long2ice <long2ice@gmail.com>
2021-03-16 15:57:53 +0800

    add language_level in build

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ from distutils.command.build_ext import build_ext
 
 def build(setup_kwargs):
     setup_kwargs.update({
-        'ext_modules': cythonize(["asyncmy/*.pyx"]),
-        'cmdclass': {'build_ext': build_ext}
+        "ext_modules": cythonize(["asyncmy/*.pyx"]),
+        "cmdclass": {"build_ext": build_ext},
+        "language_level": 3
     })
